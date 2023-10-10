c34347a20fe0e85d57545be7ed91445cd3c50a6a
Christian Mortensen <christian@svalgaard.net>
2023-10-10 17:09:45 +0200

    Blackjack
    Add a card to the deck function

--- funFocus/blackjack.c
+++ funFocus/blackjack.c
@@ -3,42 +3,46 @@
 #include <time.h>
 #define SHUFFLES 100
 
-typedef struct Card
+typedef enum
 {
-	enum Rank
-	{
-		two = 2,
-		three,
-		four,
-		five,
-		six,
-		seven,
-		eight,
-		nine,
-		ten,
-		jack,
-		queen,
-		king,
-		ace
-	} rank;
-	enum Suit
-	{
-		heart,
-		spade,
-		club,
-		diamond
-	} suit;
+	two = 2,
+	three,
+	four,
+	five,
+	six,
+	seven,
+	eight,
+	nine,
+	ten,
+	jack,
+	queen,
+	king,
+	ace
+} Rank;
 
+typedef enum
+{
+	heart,
+	spade,
+	club,
+	diamond
+} Suit;
+
+typedef struct
+{
+	Rank rank;
+	Suit suit;
 } Card;
 
-typedef struct Deck
+typedef struct
 {
 	Card *cards;
 	int size;
 } Deck;
 
-Deck createDeck(Deck);
+Deck createDeck();
 void shuffleDeck(Deck *);
+void addCard(Deck *deck, Card card);
 void printDeck(Deck);
 void start();
 
@@ -47,18 +51,24 @@ int main(void)
 	start();
 	Deck deck = createDeck(deck);
 	shuffleDeck(&deck);
+	Card newCard;
+	newCard.suit = heart;
+	newCard.rank = four;
+	addCard(&deck, newCard);
 	printDeck(deck);
 
 	free(deck.cards); // Is also run when the program is terminated so in this case it dosent make a difference
 	return 0;
-}	
+}
 
-void start(){
+void start()
+{
 	srand(time(NULL));
 }
 
-Deck createDeck(Deck deck)
+Deck createDeck()
 {
+	Deck deck;
 	deck.cards = (Card *)malloc(52 * sizeof(Card));
 	deck.size = 0;
 	for (int suit = 0; suit < 4; suit++)
@@ -70,6 +80,7 @@ Deck createDeck(Deck deck)
 			deck.size++;
 		}
 	}
+	return deck;
 }
 
 void shuffleDeck(Deck *deck)
@@ -91,4 +102,13 @@ void printDeck(Deck deck)
 		Card card = deck.cards[i];
 		printf("Card %2d: Rank %2d, Suit %d\n", i + 1, card.rank, card.suit);
 	}
+}
+
+void addCard(Deck *deck, Card card)
+{
+	deck->cards = (Card *)realloc(deck->cards, (deck->size + 1) * sizeof(Card));
+
+	deck->cards[deck->size].suit = card.suit;
+	deck->cards[deck->size].rank = card.rank;
+	deck->size++;
 }
