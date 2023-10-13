e0a199bf538eb509c79eb92ad9d6d6526983de5d
Christian Mortensen <christian@svalgaard.net>
2023-10-13 12:23:32 +0200

    More Lesson 6

--- imperativProgrammering/aau-6-3.c
+++ imperativProgrammering/aau-6-3.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 
+double multi_apply(int n, double s);
+double half(double x);
 
-int main(void){
-
-	
-
-
+int main(void)
+{
+	printf("Result : %.2lf", multi_apply(3, 16));
 	return 0;
+}
+
+double multi_apply(int n, double s)
+{
+	s = half(s);
+	n--;
+	if(n>0){	
+		return (multi_apply(n,s));
+	}
+	return s;
+}
+
+double half(double x){
+    return x/2;
 }
