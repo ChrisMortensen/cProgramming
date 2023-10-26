506b1d20a854b437b82c7558983de9530a506d82
Christian Mortensen <christian@svalgaard.net>
2023-10-26 13:40:45 +0200

    Eksamensopgave 1
    Last changes before handing in the assignment

--- eksamensopgaver/calculator.c
+++ eksamensopgaver/calculator.c
@@ -1,6 +1,6 @@
 /*
 	Navn			:	Christian Svalgaard Mortensen
-	Email			:	christian@svalgaard.net
+	Email			:	csmo23@student.aau.dk
 	Gruppe			:	1
 	Studieretning	:	Cyber- og computer teknologi
 */
@@ -8,16 +8,32 @@
 #include <stdio.h>
 #include <math.h>
 
-void scan_data(char *operator, double * operand);
-void do_next_op(double *accumulator, char operator, double operand);
-double run_calculator();
-int isOperatorBinary(char operator);
+double run_calculator();					//	Run the calculator
+void scan_data(char *, double *);			//	Scan operator and operand
+int isOperatorBinary(char);					//	Check if operator is binary
+void do_next_op(double *, char, double);	//	Perform the next operation
 
 int main(void)
 {
 	double result = run_calculator();
 	printf("Final result is: %.6lf", result);
 	return 0;
+}
+
+double run_calculator()
+{
+	double accumulator = 0.0, operand = 0.0;
+	char operator;
+	do
+	{
+		scan_data(&operator, & operand);
+		if (operator== 'q')
+		{
+			return accumulator;
+		}
+		do_next_op(&accumulator, operator, operand);
+		printf("Result so far is %.6lf.\n", accumulator);
+	} while (1);
 }
 
 void scan_data(char *operator, double * operand)
@@ -37,7 +53,7 @@ void scan_data(char *operator, double * operand)
 
 int isOperatorBinary(char operator)
 {
-	return (operator== '+' || operator== '-' || operator== '*' || operator== '/' || operator== '^');
+	return (operator == '+' || operator == '-' || operator == '*' || operator == '/' || operator == '^');
 }
 
 void do_next_op(double *accumulator, char operator, double operand)
@@ -47,53 +63,45 @@ void do_next_op(double *accumulator, char operator, double operand)
 	case '+':
 		*accumulator += operand;
 		break;
+
 	case '-':
 		*accumulator -= operand;
 		break;
+
 	case '*':
 		*accumulator *= operand;
 		break;
+
 	case '/':
 		if (operand != 0.0)
 		{
 			*accumulator /= operand;
 		}
 		break;
+
 	case '^':
 		*accumulator = pow(*accumulator, operand);
 		break;
+		
 	case '#':
 		if (*accumulator >= 0.0)
 		{
 			*accumulator = sqrt(*accumulator);
 		}
 		break;
+
 	case '%':
 		*accumulator = -(*accumulator);
 		break;
+
 	case '!':
 		if (*accumulator != 0.0)
 		{
 			*accumulator = 1.0 / *accumulator;
 		}
 		break;
+
 	default:
 		break;
 	}
-}
-
-double run_calculator()
-{
-	double accumulator = 0.0, operand = 0.0;
-	char operator;
-	do
-	{
-		scan_data(&operator, & operand);
-		if (operator== 'q')
-		{
-			return accumulator;
-		}
-		do_next_op(&accumulator, operator, operand);
-		printf("Result so far is %.6lf.\n", accumulator);
-	} while (1);
 }
